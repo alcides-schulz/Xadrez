946a4e3cfb1cff56ba4572db313bbb56de73059b
Alcides Schulz <alcides_schulz@hotmail.com>
2018-03-12 20:00:17 -0400

    Update Programa.cs documentation

--- CodigoFonte/Programa.cs
+++ CodigoFonte/Programa.cs
@@ -20,12 +20,12 @@ namespace Enxadrista
     /// Você pode instalar o Enxadrista.exe como um novo "engine" em um desses programas gráficos.
     /// Em seguida, você joga contra a Enxadrista, ou pode fazê-lo jogar com outros programas já instalados.
     /// 
-    /// Para começar, você usará os comandos de linha que podem ser digitados quando
-    /// você inicia Enxadrista.exe. A sintaxe dos comandos dependerá do protocolo que você usa.
+    /// Se você executar o Enxadrista.exe, você pode dar comandos diretamente ao mecanismo. A sintaxe dos
+    /// comandos dependerá do protocolo que você usa.
     /// Existem dois protocolos que podem ser usados: XBoard e UCI. Os programas gráficos geralmente 
     /// aceitam ambos os protocolos. 
-    /// Enxadrista é desenvolvido para usar o Xboard, também conhecido como Winboard. Na fonte do protocolo 
-    /// "Xboard.cs", há uma discussão sobre os dois protocolos.
+    /// Enxadrista é desenvolvido para usar o Xboard, também conhecido como Winboard. No fonte do protocolo 
+    /// "Xboard.cs", você tem mais detalhes.
     /// 
     /// Tabuleiro - Board - Tabuleiro.cs
     /// --------------------------------
@@ -39,26 +39,31 @@ namespace Enxadrista
     /// -------------------------------
     /// Executa a busca pelo melhor movimento. Esta busca é baseada na força bruta, mas com várias técnicas 
     /// que evitam olhar para todos os movimentos.
-    /// A forma mais comum usada na pesquisa é a pesquisa alfa/beta.
+    /// A forma mais comum é a pesquisa alfa/beta com variação principal (alpha/beta principal variation 
+    /// search).
     /// Algumas técnicas utilizadas durante a pesquisa, que foram programadas neste programa, são:
-    /// - pesquisa de variação principal - principal variation search (PVS)
     /// - pesquisa de janela zero - zero window search
     /// - movimento nulo - null move
     /// - tabela de transposição - tranposition table
     /// - pesquisa quiescente - quiesce search
     /// - poda de futilidade - futility pruning
     /// - redução de movimento tardio - late move reduction
+    /// - e outras.
     /// 
     /// Avaliacao - Evaluation - Avaliacao.cs
     /// -------------------------------------
     /// Avalia e gera um valor para a posição no tabuleiro de xadrez. Esta função é usada pela procura.
     /// Deve levar em consideração vários fatores para indicar qual lado tem uma vantagem. 
-    /// Assim, juntamente com a procura, o motor determina o movimento que levará a melhores posições.
+    /// Assim, juntamente com a procura, o motor determina o melhor. É um conceito importante, o motor 
+    /// não está procurando o melhor movimento em si, mas está procurando o movimento que leva às 
+    /// melhores posições. Claro que alguns movimentos são decisivos, mas em muitas posições, o motor 
+    /// seleciona os movimentos que têm a mais probabilidade de ganhar.
     /// Alguns termos de avaliação que podem ser usados:
     /// - material - o mais importante
     /// - segurança do rei - importante e requer trabalho para funcionar bem
     /// - estrutura do peão - peões isolados, peões duplos, peões passados, etc.
     /// - mobilidade das peças - movimentos disponíveis 
+    /// - etc.
     /// 
     /// Como o motor joga xadrez
     /// ------------------------
@@ -67,10 +72,10 @@ namespace Enxadrista
     /// A procura consiste em gerar todos movimentos para a posição e para cada movimento, gerar e 
     /// executar cada resposta. Este processo é repetido recursivamente para cada movimento e é limitado 
     /// por tempo ou profundidade. 
-    /// A profundidade é o número de movimentos que o programa poderá alcançar. Por examplo a profundidade
+    /// A profundidade é o número de movimentos que o programa poderá alcançar. Por exemplo a profundidade
     /// 8 indica que o programa ve 8 movimentos adiante (4 movimentos do lado branco, e 4 do lado preto).
     /// O programa irá então avaliar a posição para dar um valor e, em seguida, selecionar os movimentos 
-    /// que levam a boas posições.
+    /// que levam a melhores posições.
     /// Um aspecto importante é que o programa de xadrez sempre considera a melhor resposta do adversário 
     /// durante a procura.
     /// 
@@ -97,25 +102,28 @@ namespace Enxadrista
     ///   furo no casco. Pode levar tempo, mas eventualmente ele vai afundar.
     /// - Velocidade: um programa de xadrez visita milhões de posições por segundo, precisa ser rápido. E o mais 
     ///   importante é que a estrutura do programa seja bem definida, tendo um bom desempenho como um todo. Não adianta 
-    ///   otimizar uma pequena função do programa. Mas certifique que o programa funciona corretamente antes de tentar 
+    ///   otimizar uma pequena função do programa. Mas certifique-se que o programa funciona corretamente antes de tentar 
     ///   melhorar o desempenho.
     ///   Para o seu primeiro programa, tente priorizar esta ordem: correção, clareza e desempenho
     /// - Uso de técnicas conhecidas: existem vários conceitos e técnicas que devem ser aplicadas. 
     ///   Por exemplo: movimento nulo (movimento nulo). Este é um conceito que cada programa deve implementar 
-    ///   porque aumenta significativamente a força do programa.
+    ///   porque aumenta significativamente a força do programa. 
+    ///   No início, tente se concentrar no uso de técnicas conhecidas usadas em outros programas, depois você também 
+    ///   pode tentar suas próprias idéias, essa é a melhor parte.
     /// - Disciplina e persistência: é muito difícil aumentar a força de um programa de xadrez, você precisa fazer
     ///   uma mudança de cada vez e testar com milhares de jogos rápidos e medir se a mudança surtiu algum efeito (positivo
     ///   ou negativo), então aceitar ou rejeitar. No início, é fácil medir as melhorias, mas com a passagem do tempo
     ///   essas melhorias são menores e mais dificeis de quantificar. 
-    ///   Então você deve estabelecer seu processo de desenvolvimento. O software de controle de código-fonte ajuda muito.
+    ///   Então você deve estabelecer seu processo de desenvolvimento. O software de controle de código-fonte ajuda muito, 
+    ///   tipo "git/github".
     /// 
     /// Onde obter mais informações
     /// ---------------------------
     /// - Chess Programming Wiki(https://chessprogramming.wikispaces.com/) - tem tudo sobre programação de programas de xadrez.
     /// - Fórum de programação de xadrez: www.talkchess.com é um dos mais conhecidos.
-    /// - Fontes do programa - vários autores fornecem o código-fonte de seu programas. Procure na web sempre usando o nome do 
+    /// - Fontes do programa - vários autores fornecem o código-fonte de seu programas. Procure na web, sempre usando o nome do 
     ///   programa e "chess engine". Exemplo "stockfish chess engine"
-    ///     - stockfish: este é um dos programas mais fortes do mundo, com a vantagem de ser gratis e ser código aberto.
+    ///     - stockfish: este é um dos programas mais fortes do mundo, com a vantagem de ser grátis e ter código aberto.
     ///     - crafty: um dos mais antigos e mais tradicionais e contém uma documentação detalhada das técnicas.
     ///     - fruit: bom programa, pouca documentação, mas mostra elegantemente a implementação de várias técnicas.
     ///     - tcsp: programa de ensino, consulta obrigatória para todos os iniciantes.
@@ -141,7 +149,7 @@ namespace Enxadrista
     /// são um assunto de pesquisa bem interessante (procure no google sobre AlphaZero, que também é pesquisa do google) 
     /// 
     /// PERGUNTA: Por que você não usa idéias do stockfish e outros programas e implementa no seu programa?
-    /// RESPOSTA: veja o item "Disciplina e Persistência" acima. As ideias de um programa nem sempre funcionam diretamente 
+    /// RESPOSTA: veja o item "Disciplina e Persistência" acima. As idéias de um programa nem sempre funcionam diretamente 
     /// em outro programa, a maior parte do tempo requer alguma persistência para fazer funcionar. Você pode usar 
     /// idéias como inspiração e se adaptar ao seu programa. Em geral, algo pode funcionar bem em um programa mas não em outro.
     /// 
@@ -151,7 +159,7 @@ namespace Enxadrista
     /// o maior desafio foi o gerenciamento de memória. Parece que o coletor de lixo foi bem afetado pelo tamanho 
     /// da tabela de transposição. O uso de 64 MB tornou o programa mais lento. Tive que usar uma tabela bem pequena.
     /// Uma vez que o motor de xadrez olhará para milhões de posições, devemos evitar a criação de novos objetos
-    /// durante a pesquisa. O uso do comando GC.Collect() ajudou um pouco.
+    /// durante a pesquisa. O uso do comando GC.Collect() ajudou um pouco. Pretendo rever isso no futuro.
     /// Mas o objetivo aqui foi mostrar como usar e explicar as técnicas. Provavelmente, um bom exercício 
     /// seria substituir a criação de objetos (new) e a lista dinâmica por matrizes alocadas no início.
     /// Se você for escrever um programa mais competitivo, provavelmente usará C ou C++.
